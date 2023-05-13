a0b994f5906c2270ed636bfdb6ac64a7ef98d1db
17tyDM <takehiiragi@gmail.com>
2023-05-13 14:00:08 +0900

    testcode_python

--- test.py
+++ test.py
@@ -2,3 +2,15 @@ def main():
     print("Hello Python")
 if __name__=='__main__':
     main()
+
+def test():
+    fruit_list = ["apple","banana","grape","strawberry"]
+    for value in fruit_list:
+        if value == "apple" or value == "strawberry":
+            print("赤い果物")
+        elif value == "banana":
+            print("黄色い果物")
+        else :
+            print("赤色でも黄色でもない果物")
+
+test()
